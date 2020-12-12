0ad0c1e45f808cb03390b5c861a9da53eee6e3b2
Elissa_Reiskind <elissa.reiskind@gmail.com>
2020-12-12 02:30:56 +0000

    Define first two routes in Flask app

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/') # This defines the route off the domain
+def home(): # This is the function that executes when the browser goes to the route above
+    return "home"
+
+@app.route('/about')
+def about():
+    return "about page"
